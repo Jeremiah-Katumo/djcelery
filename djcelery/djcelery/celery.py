678a085741128dc079073fe61cfdee3b5979b63a
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-29 14:21:50 +0300

    starting a celery worker

--- djcelery/djcelery/celery.py
+++ djcelery/djcelery/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcelery.settings')
 
 app = Celery("djcelery")
-app.conf.from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 @app.task
 def add_numbers(x, y):
